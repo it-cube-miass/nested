da91a45791f287c5c2154ef15398581545d062d1
haschish <haschish@gmail.com>
2020-02-06 14:32:50 +0500

    changed inner.Main

--- src/inner/Main.java
+++ src/inner/Main.java
@@ -5,8 +5,13 @@ public class Main {
 
         Account myAccount = new Account("Big Mazzy", "e89b-12d3-a456");
         Card myCard = new Card("1034-9383-3948-3498", myAccount);
+        Card wifeCard = new Card("3344-7732-8673-8885", myAccount);
+
+        System.out.println(myAccount.getAmount());
 
         myCard.withdraw(-1000);
+        wifeCard.withdraw(3000);
+
         System.out.println(myAccount.getAmount());
     }
 }
